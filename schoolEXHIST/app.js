3ca44d3c7570c91955f58a3e8735966576e89c1f
yuvcarl <carldrake969@gmail.com>
2024-01-19 15:04:01 +0100

    updated aded the app.js file

---
+++ schoolEXHIST/app.js
@@ -0,0 +1,22 @@
+
+
+
+function writeValue(param){
+    var btn = document.getElementById("btn")
+
+    var writein = document.getElementById('writein')
+
+    var newValue = writein.value + param
+    writein.value = newValue
+
+    //alert(param)
+}
+
+function EvaluteA(){
+    var writein = document.getElementById('writein')
+    var answer = eval(writein.value)
+    var answerDisplay = document.getElementById("answerDisplay")
+    answerDisplay.value = answer
+
+}
+
